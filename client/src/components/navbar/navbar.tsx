20008cf26d1f3f458b4853807285d0b4129c9402
spawnkid1987 <borisisac@gmail.com>
2026-03-05 01:34:42 +0000

    [ADD] navbar

---
+++ client/src/components/navbar/navbar.tsx
@@ -0,0 +1,45 @@
+import React from "react"
+import styles from "./navbar.module.css"
+
+export const Navbar: React.FC = () => {
+    interface NavLink {
+        name: string;
+        href: string;
+    }
+
+    const navLinks: NavLink[] = [
+        { name: "Home", href: "/" },
+        { name: "Services", href: "/services" },
+        {name: "Projects", href: "/projects" },
+        { name: "About Us", href: "/about" },
+        { name: "Contacts", href: "/contact" }
+    ];
+
+
+    return (
+
+    <div className={styles.container}>
+        <nav className={styles.navbar}>
+            <div className={styles.logo_section}>
+                <a href="/">
+                    <img src="/src/assets/logo2.png" alt="Logo" />
+                </a>
+             </div>
+            <div className={styles.links_section}>
+                
+                <ul className={styles.nav_links}>
+                    {navLinks.map((link, index) => (
+                        <li key={index}>
+                            <a href={link.href}>{link.name}</a>
+                        </li>
+                    ))}
+                </ul>
+                <div className={styles.button_section}>
+                    <button>Get in Touch</button>
+                </div>
+            </div>            
+        </nav>
+    </div>
+
+  )
+}
